1260ccb87d7e866bdb9ec8559ddfb72acf7e5783
Kacper <contact@vg-electronics.com>
2026-03-11 15:33:50 +0100

    redirect from '/' to '/countries'

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,6 +3,7 @@ import Country from '../components/Country.vue';
 import Countries from "../components/Countries.vue";
 
 const routes = [
+  { path: '/', redirect: '/countries' },
   { path: '/countries', name: 'dashboard', component: Countries },
   { path: '/countries/:name', name: 'country', component: Country, props: true },
 ];
